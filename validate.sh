114c83700b07f8b29b356a38847ee5338c9f8ed2
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-03-14 11:38:17 +0100

    Update validate.sh

--- validate.sh
+++ validate.sh
@@ -3,8 +3,18 @@
 echo "deb http://deb.debian.org/debian/ sid main" | sudo tee -a /etc/apt/sources.list
 
 menu () {
-   echo "Do you want update your kernel"
+   echo "Menu : Do you want update your kernel"
+   case $selection in
+  1 ) clear ; menu_option_one ; yes ;;
+  2 ) clear ; menu_option_two ; no ;;
+  * ) clear ; incorrect_selection ; press_enter ;;
+esac
 }
 
 
+
+
+
+
+   
 menu
