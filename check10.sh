48636206215830948a70593507747e0d65dfb625
Cazeho <58745332+Cazeho@users.noreply.github.com>
2022-03-15 16:48:36 +0100

    Update check10.sh

--- check10.sh
+++ check10.sh
@@ -3,4 +3,24 @@
 #Debian 10
 
 
+kernel=$1
+ver1=$(echo ${kernel:-$(uname -v | awk '{print $4}' | cut -d '-' -f1)} | cut -d '.' -f1)
+ver2=$(echo ${kernel:-$(uname -v | awk '{print $4}' | cut -d '-' -f1)} | cut -d '.' -f2)
+ver3=$(echo ${kernel:-$(uname -v | awk '{print $4}' | cut -d '-' -f1)} | cut -d '.' -f3)
+echo $ver1 $ver2 $ver3
 
+
+if (( ${ver1:-0} < 5 )) ||
+   (( ${ver1:-0} == 5 && ${ver2:-0} < 8 )) ||
+   (( ${ver1:-0} == 5 && ${ver2:-0} == 10 && ${ver3:-0} == 102 )) ||
+   (( ${ver1:-0} == 5 && ${ver2:-0} == 10 && ${ver3:-0} == 92 )) ||
+   (( ${ver1:-0} == 5 && ${ver2:-0} == 15 && ${ver3:-0} == 25 )) ||
+   (( ${ver1:-0} == 5 && ${ver2:-0} >= 16 && ${ver3:-0} >= 11 )) ||
+   (( ${ver1:-0} == 5 && ${ver2:-0} > 16 ));
+then
+    echo Not vulnerable
+    exit 0
+else
+    echo Vulnerable
+    exit 1
+fi
